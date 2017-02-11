c11b67119215e74a98e50ca0cb764ed757c94e5d
Paul Dewost <paul.dewost@gmail.com>
2017-02-11 19:19:05 +0100

    Slight bug due to Charlie...

--- tabs/src/app/app.component.ts
+++ tabs/src/app/app.component.ts
@@ -13,18 +13,19 @@ export class MyApp {
 
   rootPage = LoginPage;
 
-  constructor(platform: Platformn public auth:Auth) {
+  constructor(platform: Platform) { // public auth:Auth
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       StatusBar.styleDefault();
       Splashscreen.hide();
-
+      /*
       if(this.auth.isAuthenticated()) {
         this.rootPage = HomePage;
       } else {
         this.rootPage = LoginPage;
       }
+      */
     });
   }
 }
