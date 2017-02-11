3b67a15f49b4e4865187f579383d13b13a11329c
Charlie <charlie.naudy@gmail.com>
2017-02-11 23:22:59 +0100

    login&password in the pocket

--- tabs/src/pages/login/login.ts
+++ tabs/src/pages/login/login.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { AlertController } from 'ionic-angular';
 
 import { NavController } from 'ionic-angular';
 import { TabsPage } from '../tabs/tabs';
 import { RegisterPage } from '../register/register';
+import { Http } from '@angular/http';
 
 @Component({
   selector: 'page-login',
@@ -10,11 +12,20 @@ import { RegisterPage } from '../register/register';
 })
 
 export class LoginPage {
+  email = null;
+  password=null;
 
-  constructor(public navCtrl: NavController) {}
+  constructor(public navCtrl: NavController, public alertCtrl: AlertController) {}
 
   login() {
-    this.navCtrl.setRoot(TabsPage);
+    //this.navCtrl.setRoot(TabsPage);
+    let alert = this.alertCtrl.create({
+      title: 'It works bitch!',
+      subTitle: 'email: '+this.email+'  password: '+this.password,
+      buttons: ['OK']
+    });
+    alert.present();
+
   }
   to_register() {
     this.navCtrl.push(RegisterPage);
