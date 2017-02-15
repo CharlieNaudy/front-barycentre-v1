6739cda7ba891333adcb5e63280cb4a76ae84418
Paul Dewost <paul.dewost@gmail.com>
2017-02-15 19:05:31 +0100

    Login and register page fully operational.

--- tabs/src/pages/login/login.ts
+++ tabs/src/pages/login/login.ts
@@ -20,13 +20,15 @@ import { SecureStorage } from 'ionic-native';
 
 export class LoginPage {
 
-  email: string;
-  password: string;
   url: string;
   secureStorage: SecureStorage;
+  email: string;
+  password: string;
+
 
   constructor(public navCtrl: NavController, public alertCtrl: AlertController, public http: Http) {
     this.url = 'http://localhost:3000/users/login';
+    // POUR UTILISER LE SECURE STORAGE NATIF
     // this.secureStorage = new SecureStorage();
     // this.secureStorage.create('Secure storage').then(
     //   () => console.log('Storage is ready!'),
@@ -42,30 +44,41 @@ export class LoginPage {
       );
   }
 
+  getRegister() {
+    this.navCtrl.push(RegisterPage);
+  }
+
   login() {
-    return this.http.post(this.url, {
+    this.http.post(this.url, {
       email: this.email,
       password: this.password
     }).map((res: Response) => res.json()).subscribe(messageJson => {
       if (messageJson.error) {
-        let alert = this.alertCtrl.create({ title: 'Error', subTitle: messageJson.error, buttons: ['OK'] });
+        let alert = this.alertCtrl.create({
+          title: 'Error',
+          subTitle: messageJson.error,
+          buttons: ['OK']
+        });
         alert.present();
       } else if (messageJson.token && messageJson.userId) {
         window.localStorage['userId'] = messageJson.userId;
         window.localStorage['token'] = messageJson.token;
-        window.localStorage['login'] = this.email;
         // this.createStorage();
-        let alert = this.alertCtrl.create({ title: 'Login', subTitle: 'Welcome to Barycentre !', buttons: ['OK'] });
+        let alert = this.alertCtrl.create({
+          title: 'Login',
+          subTitle: 'Welcome to Barycentre !',
+          buttons: ['OK']
+        });
         alert.present();
         this.navCtrl.setRoot(TabsPage);
       } else {
-        let alert = this.alertCtrl.create({ title: 'Error', subTitle: 'Something went wrong.', buttons: ['OK'] });
+        let alert = this.alertCtrl.create({
+          title: 'Error',
+          subTitle: 'Something went wrong.',
+          buttons: ['OK']
+        });
         alert.present();
       }
     });
-  }
-
-  getRegister() {
-    this.navCtrl.push(RegisterPage);
   }
 }
--- tabs/src/pages/register/register.ts
+++ tabs/src/pages/register/register.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { AlertController } from 'ionic-angular';
 
-
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController } from 'ionic-angular';
 import { TabsPage } from '../tabs/tabs';
-import { Http } from '@angular/http';
+
+import { Http, Response } from '@angular/http';
+import { Observable } from 'rxjs/Rx';
+import 'rxjs/add/operator/map';
+
+import { SecureStorage } from 'ionic-native';
 
 @Component({
   selector: 'page-register',
@@ -13,50 +17,89 @@ import { Http } from '@angular/http';
 
 export class RegisterPage {
 
-  firstName: string;
-  lastName: string;
-  address: string;
+  url: string;
+  secureStorage: SecureStorage;
   email: string;
-  password1: string;
-  password2: string;
+  password1: string; // TODO: vérifier
+  password2: string; // l'égalité en dynamique
+  firstName: string; // TODO : désactiver le boutton "register"
+  lastName: string;  // tant que tous les champs ne sont pas remplis
+  address: string;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public alertCtrl: AlertController, public http: Http) {
+  constructor(public navCtrl: NavController, public alertCtrl: AlertController, public http: Http) {
+    this.email = "";
     this.password1 = "";
     this.password2 = "";
+    this.firstName = "";
+    this.lastName = "";
+    this.address = "";
+    this.url = 'http://localhost:3000/users';
+    // POUR UTILISER LE SECURE STORAGE NATIF
+    // this.secureStorage = new SecureStorage();
+    // this.secureStorage.create('Secure storage').then(
+    //   () => console.log('Storage is ready!'),
+    //   error => console.log(error)
+    // );
+  }
+
+  createStorage() {
+    this.secureStorage.create('Secure storage')
+      .then(
+      () => console.log('Storage is ready!'),
+      error => console.log(error)
+      );
   }
 
   register() {
-
-    if (this.password1 === "" && this.password2 === "") {
-      let alert = this.alertCtrl.create({ title: 'Erreur', subTitle: 'Vous devez choisir un mot de passe', buttons: ['OK'] });
+    if ((this.password1 === "" && this.password2 === "") || this.email === "" || this.firstName === "" || this.lastName === "" || this.address === "") {
+      let alert = this.alertCtrl.create({
+        title: 'Error',
+        subTitle: 'All fields are mandatory',
+        buttons: ['OK']
+      });
       alert.present();
     } else if (this.password1 === this.password2) {
-      console.log('Entered the loop !');
-      this.http.post('http://localhost:3000/users', {
+      this.http.post(this.url, {
+        email: this.email,
+        password: this.password1,
         firstName: this.firstName,
         lastName: this.lastName,
-        address: this.address,
-        email: this.email,
-        password: this.password1
-      }, function(messageJson) {
+        address: this.address
+      }).map((res: Response) => res.json()).subscribe(messageJson => {
         if (messageJson.error) {
-          let alert = this.alertCtrl.create({ title: 'Erreur', subTitle: messageJson.error, buttons: ['OK'] });
+          let alert = this.alertCtrl.create({
+            title: 'Error',
+            subTitle: messageJson.error,
+            buttons: ['OK']
+          });
           alert.present();
-        } else if (messageJson.token) {
-          window.localStorage['login'] = this.email;
-          window.localStorage['authToken'] = messageJson.token;
-          let alert = this.alertCtrl.create({ title: 'Succès', subTitle: 'Connexion réussie', buttons: ['OK'] });
+        } else if (messageJson.token && messageJson.userId) {
+          window.localStorage['userId'] = messageJson.userId;
+          window.localStorage['token'] = messageJson.token;
+          // this.createStorage();
+          let alert = this.alertCtrl.create({
+            title: 'Register',
+            subTitle: 'Welcome to Barycentre !',
+            buttons: ['OK']
+          });
           alert.present();
           this.navCtrl.setRoot(TabsPage);
+        } else {
+          let alert = this.alertCtrl.create({
+            title: 'Error',
+            subTitle: 'Something went wrong.',
+            buttons: ['OK']
+          });
+          alert.present();
         }
-      })
+      });
     } else {
-      let alert = this.alertCtrl.create({ title: 'Erreur', subTitle: 'Entrez deux fois le même mot de passe', buttons: ['OK'] });
+      let alert = this.alertCtrl.create({
+        title: 'Error',
+        subTitle: 'Both passwords must be the same !',
+        buttons: ['OK']
+      });
       alert.present();
     }
   }
-
-  // ionViewDidLoad() {
-  //   console.log('ionViewDidLoad RegisterPage');
-  // }
 }
