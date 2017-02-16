46955547ecaaaa71b35f10758cd4aac8f90c6aa6
Paul Dewost <paul.dewost@gmail.com>
2017-02-16 20:58:04 +0100

    Implementing friends page.

--- tabs/src/pages/tabs/tabs.ts
+++ tabs/src/pages/tabs/tabs.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
-import { ContactPage } from '../contact/contact';
+import { FriendsPage } from '../friends/friends';
 import { LoginPage } from '../login/login';
 import { MeetingsPage } from '../meetings/meetings';
 import { SettingsPage } from '../settings/settings';
@@ -16,7 +16,7 @@ export class TabsPage {
   // should be each tab's root Page
 
   tab1Root: any = MeetingsPage;
-  tab2Root: any = ContactPage;
+  tab2Root: any = FriendsPage;
   tab3Root: any = SettingsPage;
 
   constructor(public navCtrl: NavController) { }
