ee6234390498853389bea3c45062e9e8a4366271
Charlie <charlie.naudy@gmail.com>
2017-02-16 20:49:00 +0100

    friends

--- tabs/src/pages/tabs/tabs.ts
+++ tabs/src/pages/tabs/tabs.ts
@@ -15,15 +15,9 @@ export class TabsPage {
   // this tells the tabs component which Pages
   // should be each tab's root Page
 
-<<<<<<< HEAD
-  tab1Root: any = HomePage;
-  tab2Root: any = AboutPage;
-  tab3Root: any = FriendsPage;
-=======
   tab1Root: any = MeetingsPage;
   tab2Root: any = ContactPage;
   tab3Root: any = SettingsPage;
->>>>>>> 3aa3722db36c73a3d63a62a8cc07b4924abad0af
 
   constructor(public navCtrl: NavController) { }
 }
